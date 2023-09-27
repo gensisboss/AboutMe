3493d31ce4e6952af1b59719e368c6142b438ffc
gongganghao <2908273068@qq.com>
2023-09-27 11:43:53 +0800

    Add feature # 完善界面

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,63 @@
 function setActive(id) {
-    const menuItems = document.getElementsByClassName('menu-item');
-    
-    for(let i = 0; i < menuItems.length; i++) {
-       menuItems[i].classList.remove('active');
-    }
+   const menuItems = document.getElementsByClassName('menu-item');
 
-    const targetItem = document.getElementById(id);
-    targetItem.classList.add('active');
- }
+   for (let i = 0; i < menuItems.length; i++) {
+      menuItems[i].classList.remove('active');
+   }
 
- function leaveMessage() {
-    var message = document.getElementById('exampleFormControlTextarea1').value;
-    alert(message);
- }
+   const targetItem = document.getElementById(id);
+   targetItem.classList.add('active');
+}
 
-
- function randomText(){
-    var text = ("abcdefghijklmnopqrstuvwxyz0123456789");
-    let letter = text[Math.floor(Math.random()*text.length)];
-    return letter
- }
-
- function rain(){
-    let cloud = document.querySelector('.cloud')
-    let e = document.createElement('div')
-    let left = Math.floor(Math.random()*320);
-    let size = Math.random()*1.5;
-    e.classList.add('text');
-    e.innerText = randomText();
-    e.style.left = left + 'px'
-    e.style.fontSize = 0.5+size+'em'
-    cloud.appendChild(e);
-
-    setTimeout(function(){
-        cloud.removeChild(e)
-    },2000)
- }
+function leaveMessage() {
+   const elements = document.getElementsByName("mycontact");
+   elements.forEach(function (element) {
+      // 在这里给元素添加动画效果
+      // 例如: 使用 CSS 动画类
+      element.classList.add('my-animation-contact');
+   });
+   var message = "对不起，我没搭建服务器，请通过左边的联系方式联系我！";
+   alert(message);
+}
 
 
+function randomText() {
+   var text = ("abcdefghijklmnopqrstuvwxyz0123456789");
+   let letter = text[Math.floor(Math.random() * text.length)];
+   return letter
+}
+
+function rain() {
+   let cloud = document.querySelector('.cloud')
+   let e = document.createElement('div')
+   let left = Math.floor(Math.random() * 320);
+   let size = Math.random() * 1.5;
+   e.classList.add('text');
+   e.innerText = randomText();
+   e.style.left = left + 'px'
+   e.style.fontSize = 0.5 + size + 'em'
+   cloud.appendChild(e);
+
+   setTimeout(function () {
+      cloud.removeChild(e)
+   }, 2000)
+}
 
 
- window.addEventListener('scroll', function() {
-    const menuItems = document.getElementsByClassName('menu-item');
-    let scrollPos = window.scrollY+100;
-    for(let i = 0; i < menuItems.length; i++) {
-        let currLink = menuItems[i];
-        let refElement = document.querySelector(currLink.getAttribute("href"));
-        if (refElement.offsetTop <= scrollPos && (refElement.offsetTop + refElement.offsetHeight > scrollPos)) {
-            // 添加活动类
-            currLink.classList.add('active');
-        } else {
-            // 删除活动类
-            currLink.classList.remove('active');
-        }
-    }
- })
+
+
+window.addEventListener('scroll', function () {
+   const menuItems = document.getElementsByClassName('menu-item');
+   let scrollPos = window.scrollY + 100;
+   for (let i = 0; i < menuItems.length; i++) {
+      let currLink = menuItems[i];
+      let refElement = document.querySelector(currLink.getAttribute("href"));
+      if (refElement.offsetTop <= scrollPos && (refElement.offsetTop + refElement.offsetHeight > scrollPos)) {
+         // 添加活动类
+         currLink.classList.add('active');
+      } else {
+         // 删除活动类
+         currLink.classList.remove('active');
+      }
+   }
+})
