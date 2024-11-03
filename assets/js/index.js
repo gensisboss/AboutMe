eb7c444184ce8da93dad06aefe052dcb24e45aff
gensisboss <2908273068@qq.com>
2024-11-03 14:39:15 +0800

    添加3d模型

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,28 +20,47 @@ function leaveMessage() {
    alert(message);
 }
 
+function model(){
+   //创建一个场景
+   const scene = new THREE.Scene();
+   scene.background = new THREE.Color(0x0a0a0a); 
+   //添加灯光
+   const spotLight = new THREE.SpotLight(0xffffff);
+   spotLight.position.set(-10,10,90);
+   scene.add(spotLight);
 
-function randomText() {
-   var text = ("abcdefghijklmnopqrstuvwxyz0123456789");
-   let letter = text[Math.floor(Math.random() * text.length)];
-   return letter
+   //创建一个相机，视点
+   const camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
+   //设置相机的位置
+   camera.position.set(0,0,0)
+   //创建一个渲染器
+   const renderer= new THREE.WebGLRenderer();
+   //设置渲染器尺寸
+   renderer.setSize(400,400);
+   document.getElementById('3dScene').appendChild(renderer.domElement);
+  
+
+  const objLoader = new THREE.OBJLoader();
+  objLoader.load("./assets/models/mars.obj",(obj)=>{
+    scene.add(obj)
+    obj.position.set(12,-6,0)
+    obj.rotation.y = 100;
+  })
+
+  let angle = 0;
+  const radius = 20;
+   const animation = ()=>{
+       requestAnimationFrame(animation);
+       angle += 0.01;
+       // camera.position.x = radius*Math.cos(angle);
+       // camera.position.z = radius*Math.sin(angle);
+       camera.lookAt(new THREE.Vector3( 0, 0, 0 ));
+       renderer.render(scene,camera);
+   }
+   animation();
 }
 
-function rain() {
-   let cloud = document.querySelector('.cloud')
-   let e = document.createElement('div')
-   let left = Math.floor(Math.random() * 320);
-   let size = Math.random() * 1.5;
-   e.classList.add('text');
-   e.innerText = randomText();
-   e.style.left = left + 'px'
-   e.style.fontSize = 0.5 + size + 'em'
-   cloud.appendChild(e);
 
-   setTimeout(function () {
-      cloud.removeChild(e)
-   }, 2000)
-}
 
 
 function transformElement(x, y,element) {
